de05af7e7f1eb717123a982d979c0b6220bf1c72
bamboosir <bamboosir@live.cn>
2018-06-03 11:52:21 +0800

    add chat function
    1. add chat function

--- ClientServer/TCPServer/TCPServer.py
+++ ClientServer/TCPServer/TCPServer.py
@@ -1,29 +1,37 @@
 from socket import *
 from time import ctime
 
-HOST    = ''
-PORT    = 21567
+HOST = ''
+PORT = 21567
 BUFSIZ = 1024
-ADDR    = (HOST,PORT)
+ADDR = (HOST, PORT)
 
 tcpSerSock = socket(AF_INET, SOCK_STREAM)
 tcpSerSock.bind(ADDR)
 tcpSerSock.listen(5)
 
-
 while True:
-    print('waiting for connection')
-    tcpCliSock,addr = tcpSerSock.accept()
-    print('..connected from',addr)
+    print('waiting for connection..')
+    tcpCliSock, addr = tcpSerSock.accept()
+    print('..connected from', addr)
 
     while True:
         data = tcpCliSock.recv(BUFSIZ)
         if not data:
             break
-        data = str(data)
-        data ="{} {}".format(ctime(),data).encode(encoding="utf-8")
-        print('recv',data)
-        tcpCliSock.send(data)
+        print(ctime(),'[recv] ',addr, data)
+
+        while True:
+            data = input('>')
+            if not data:
+                print('input error, please try again')
+                continue
+            else:
+                print(ctime(), '[send]',data)
+                tcpCliSock.send(data.encode(encoding='utf-8'))
+                break
+
+print('finish')
 
 tcpCliSock.close()
 tcpSerSock.close()
