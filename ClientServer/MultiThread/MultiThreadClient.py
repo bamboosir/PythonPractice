1464a50a24e5218d022b1bfe976d9fb024352e9b
bamboosir <bamboosir@live.cn>
2018-06-05 23:32:09 +0800

    client modify

--- ClientServer/MultiThread/MultiThreadClient.py
+++ ClientServer/MultiThread/MultiThreadClient.py
@@ -9,28 +9,38 @@ ADDR = (HOST, PORT)
 
 
 def cliRsp(tcpCliSock):
+    print("cliRsp called")
     while True:
         data = input('>')
         if not data:
             continue
         tcpCliSock.send(data.encode(encoding='utf-8'))
-        print(ctime(),'[send]',data)
+        print(ctime(),'\n[send]',data)
 
 def cliRecv(tcpCliSock):
+    print("cliRecv called")
+
     while True:
         data = tcpCliSock.recv(BUFSIZ)
         if not data:
             break
-        print(ctime(), '[recv]', data)
+        print(ctime(), '\n[recv]', data)
 
 
 def startClient():
+    #HOST = str(input('please enter server IP:'))
+    PORT = 21568
+    ADDR = (HOST, PORT)
     tcpCliSock = socket(AF_INET, SOCK_STREAM)
     tcpCliSock.connect(ADDR)
     isClntRspStarted = False
 
-    recv = threading.Thread(target=cliRecv, args=tcpCliSock)
-    rsp = threading.Thread(target=cliRsp, args=tcpCliSock)
+    recv = threading.Thread(target=cliRecv, args=[tcpCliSock])
+    rsp = threading.Thread(target=cliRsp, args=[tcpCliSock])
 
+    recv.start()
+    rsp.start()
 
+if __name__ == "__main__":
+    startClient()
 
